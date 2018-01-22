551ee8a2e2be7f8c78cd7881723fd4b9669fcf86
vietanhtran1996 <tranvietanh1996@gmail.com>
2018-01-22 20:47:20 +1300

    Initial set up

---
+++ lib/PhoneBookReader.ex
@@ -0,0 +1,5 @@
+defmodule PhoneBookReader do
+  def read do
+    "Hello world"
+  end
+end
---
+++ test/PhoneBookReader_test.exs
@@ -0,0 +1,6 @@
+defmodule PhoneBookReaderTest do
+  use ExUnit.Case
+  test "greets the world" do
+    assert PhoneBookReader.read() == "Hello world"
+  end
+end
