5ccac9a0d368e84fb812504b5586cdd0a22851ec
Viet Anh Tran <viet.anhtran@myob.com>
2018-01-24 09:35:44 +1300

    Add function to verify a phone number file

--- lib/PhoneBookReader.ex
+++ lib/PhoneBookReader.ex
@@ -1,22 +1,32 @@
 defmodule PhoneBookReader do
-  def convertToContactMaps (path) do
-    File.stream!(path) 
-      |> CSV.decode!(headers: true) 
-      |> Enum.to_list
+  def convertToContactMaps(path) do
+    File.stream!(path)
+    |> CSV.decode!(headers: true)
+    |> Enum.to_list()
   end
 
-  def getPhoneNumber (contactTuples) do
-    Enum.map(contactTuples, 
-      fn (contact) -> String.replace(contact["Phone Number"], "-", "") 
-                   |> String.replace(" ", "") end)
+  def getPhoneNumber(contactTuples) do
+    Enum.map(contactTuples, fn contact ->
+      String.replace(contact["Phone Number"], "-", "")
+      |> String.replace(" ", "")
+    end)
   end
 
-  def isValidNumber(currentPhoneNumber, phoneNumberList) do 
+  def isValidNumber(currentPhoneNumber, phoneNumberList) do
     listWithoutCurrentNumber = List.delete(phoneNumberList, currentPhoneNumber)
-    Enum.all?(listWithoutCurrentNumber, fn (current) -> !String.contains?(current, currentPhoneNumber) end)
+
+    Enum.all?(listWithoutCurrentNumber, fn current ->
+      !String.contains?(current, currentPhoneNumber)
+    end)
   end
 
-  def verify (phoneNumberList) do
-    Enum.all?(phoneNumberList, fn(current) -> isValidNumber(current, phoneNumberList) end)
+  def validate(phoneNumberList) do
+    Enum.all?(phoneNumberList, fn current -> isValidNumber(current, phoneNumberList) end)
+  end
+
+  def verify(path) do
+    PhoneBookReader.convertToContactMaps(path)
+    |> PhoneBookReader.getPhoneNumber()
+    |> PhoneBookReader.validate()
   end
 end
--- test/PhoneBookReader_test.exs
+++ test/PhoneBookReader_test.exs
@@ -1,19 +1,5 @@
 defmodule PhoneBookReaderTest do
   use ExUnit.Case
-  test "get list of contact tuples" do
-    expected = [%{"Name" => "Kimberlee Turlington", "Phone Number" => "039 298-72-30"},
-                %{"Name" => "Miguel Eveland", "Phone Number" => "032-2659094"},
-                %{"Name" => "Audrie Smid", "Phone Number" => "03758295-10 32"},
-                %{"Name" => "Antonia Maslanka", "Phone Number" => "00594-83-34 93"},
-                %{"Name" => "Val Hoffmeyer", "Phone Number" => "001-859-07-84"},
-                %{"Name" => "Fleta Mckiney", "Phone Number" => "046-30-01-80"},
-                %{"Name" => "Leanna Wedel", "Phone Number" => "0184 425384"},
-                %{"Name" => "Reva Larger", "Phone Number" => "0262-2875"},
-                %{"Name" => "Shelby Prator", "Phone Number" => "0174-1599"},
-                %{"Name" => "Micheal Veronesi", "Phone Number" => "09564 310-6 4"}]
-    assert PhoneBookReader.convertToContactMaps(File.cwd! <> "/test/phone_data_small_set.txt") == expected
-  end
-
   test "get cleansed Phone Number from contact tuples" do
     expected = [ "0392987230", "0322659094", "037582951032", "00594833493"]
     contactTuples = [%{"Name" => "Kimberlee Turlington", "Phone Number" => "039 298-72-30"},
@@ -37,16 +23,16 @@ defmodule PhoneBookReaderTest do
 
   test "return true for a consistent list of phone numbers" do
     phoneNumberList = [ "0392987230", "0322659094", "037582951032", "00594833493" ]
-    assert PhoneBookReader.verify(phoneNumberList) == true
+    assert PhoneBookReader.validate(phoneNumberList) == true
   end
   
   test "return false for an inconsistent list of phone numbers" do
     phoneNumberList = ["911", "91125426", "97625992" ]
-    assert PhoneBookReader.verify(phoneNumberList) == false
+    assert PhoneBookReader.validate(phoneNumberList) == false
   end
   
   test "return false for an inconsistent list of phone numbers with mixed up order" do
     phoneNumberList = ["91125426", "911", "97625992" ]
-    assert PhoneBookReader.verify(phoneNumberList) == false
+    assert PhoneBookReader.validate(phoneNumberList) == false
   end
 end
